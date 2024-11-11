0016f2f88c024150798fb2fbe95ce4f2047c01da
Martin Stamm <martin.stamm@camunda.com>
2024-11-11 10:25:26 +0100

    feat: add monaco editor to frontend
    related to https://github.com/camunda/rpa-runtime/issues/9

---
+++ webpack.config.cjs
@@ -0,0 +1,71 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+const path = require('path');
+
+// const CamundaModelerWebpackPlugin = require('camunda-modeler-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/client'),
+    filename: 'client.js'
+  },
+  plugins: [
+
+    // new CamundaModelerWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /[/\\][A-Z][^/\\]+\.svg$/,
+        use: 'react-svg-loader'
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(bpmn|cmmn|dmn|form|robot)$/,
+        type: 'asset/source'
+      },
+
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: [ '@babel/preset-react' ]
+      //     }
+      //   }
+      // }
+    ]
+  },
+  devtool: 'cheap-module-source-map'
+};
