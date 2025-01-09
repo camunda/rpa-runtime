1f19582e5bd5ae71a4baac9ccb894144198617b5
Adam Pounder <adam.pounder@camunda.com>
2025-01-09 12:39:45 +0000

    Python module structure

---
+++ Camunda/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Camunda',
+   version='1.0',
+   description='',
+   author='',
+   author_email='noone@example.com',
+   packages=['Camunda'], 
+   install_requires=[], 
+)
